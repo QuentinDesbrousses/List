da9b84302087cb8876c3f4a6603f6b73571f0236
Quentin Desbrousses <quentin.desbrousses@etu.umontpellier.fr>
2022-02-16 13:55:36 +0100

    Initial Commit

---
+++ list/listApp.swift
@@ -0,0 +1,17 @@
+//
+//  listApp.swift
+//  list
+//
+//  Created by m1 on 16/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
